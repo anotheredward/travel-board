aab0e232fddbbad8be6c9fff9d63fc3a2a827180
Edward Robinson <proteenx11@gmail.com>
2016-05-12 15:07:10 +1200

    populate sign from api

--- sign.js
+++ sign.js
@@ -1,15 +1,39 @@
-/* global $ */
+/* global $, R */
+
+const SIGN_IDS = [334, 335, 336]
 
 
-$.get('http://cors.io/?u=http://trafficnz.info/service/traffic/rest/4/signs/tim/all',
+$.get('https://crossorigin.me/http://trafficnz.info/service/traffic/rest/4/signs/tim/all',
   function (data) {
-    console.log($.parseXML(data).find)
+    const json = $.xml2json(data)['#document']['response']
+    const signs = $.makeArray(json.tim)
+    const lines = R.map(sign => ({id: sign.id, items: getItems(sign)}), signs)
+    const convertedSigns = R.filter(x => R.contains(parseInt(x.id, 10), SIGN_IDS), lines)
+    const convertedItems = R.pipe(
+      R.uniqBy(R.prop('name')),
+      R.pluck('items'),
+      R.flatten
+    )(convertedSigns)
+
+    update(convertedItems)
   }
 )
 
-$('.travel-list').append(
-  generateRow('Airport', 30, 15)
-)
+function getItems (sign) {
+  const lines = R.path(['page', 'line'], sign)
+  if (!lines || lines.length === 0) return []
+
+  return R.pipe(
+    R.reject(x => x.left == null || x.right == null),
+    R.map(x => ({name: x.left, time: x.right}))
+  )(lines)
+}
+
+function update (items) {
+  $('.travel-list').empty()
+  const str = R.join('', R.map(item => generateRow(item.name, item.time, item.time + 5), items))
+  $('.travel-list').append(str)
+}
 
 function generateRow (name, previousTime, currentTime) {
   return `<tr>
