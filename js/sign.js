31c075b462b9f6cd28050c817fd778e756c3f41b
Edward Robinson <proteenx11@gmail.com>
2016-05-23 10:11:53 +1200

    add load from local storage

--- js/sign.js
+++ js/sign.js
@@ -1,16 +1,23 @@
 /* global $, R */
 
 var DELAY = 30 * 1000
-var currentRows = []
+var currentRows = JSON.parse(localStorage.getItem('currentRows')) || []
+var firstLoad = true
 
 update()
 window.setInterval(update, DELAY)
 
 function update () {
+  if (firstLoad && currentRows.length) {
+    render([], currentRows)
+    firstLoad = false
+  }
+
   getRows(function(newRows) {
     if (!R.equals(currentRows, newRows)) {
       render(currentRows, newRows)
       currentRows = newRows
+      localStorage.setItem('currentRows', JSON.stringify(currentRows))
     }
   })
 }
